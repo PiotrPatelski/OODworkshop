d3f9a7a10aea1dd42276947c4222dba222857a87
Krzysztof Dul <krzysztof.dul@nokia.com>
2023-01-25 11:17:46 +0100

    KD

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 #include <vector>
 
-enum class Square{
-    penalty,
-    start,
-    reward
+enum class Square { penalty, start, reward };
+
+struct Piece {
+  int position{};
 };
 
-struct Board{
+struct Board {
 public:
-    Board(){
-        squares.push_back(Square::start);
-        for(int i = 1; i < 40; i++)
-        {
-            if(i % 2 == 0)
-                squares.push_back(Square::penalty);
-            else
-                squares.push_back(Square::reward);   
-        }
+  Board() {
+    squares.push_back(Square::start);
+    for (int i = 1; i < 40; i++) {
+      if (i % 2 == 0)
+        squares.push_back(Square::penalty);
+      else
+        squares.push_back(Square::reward);
     }
-    std::vector<Square> squares;
+  }
+  std::vector<Square> squares;
+
 private:
 };
 
@@ -28,8 +28,8 @@ struct Player {
 public:
   Player(std::string name) : name(name) {}
 
-private:
   std::string name;
+  Piece piece;
   int money{};
 };
 
@@ -39,13 +39,25 @@ public:
 
   void addPlayer(std::string playerName) { players.push_back(playerName); }
 
+  void makeTurn() {
+    for (auto player : players) {
+      std::cout << "Player " << player.name << "plays. Type dice value: ";
+      int diceValue, diceValue2;
+      std::cin >> diceValue >> diceValue2;
+      // TO DO STH - update board
+      printPlayerState(player);
+    }
+  }
+
 private:
   std::vector<Player> players;
   Board board;
+  void printPlayerState(const Player &player) {}
 };
 
 int main() {
   MonopolyGame monopolygame;
   monopolygame.addPlayer("PlayerOne");
   monopolygame.addPlayer("PlayerTwo");
+  monopolygame.makeTurn();
 }
